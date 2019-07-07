7bef1db013c1b51af2cf7bab2f2665bc4afb219a
Alan Fidelino <afidedev26@gmail.com>
2019-07-07 23:03:50 +1000

    Updates

---
+++ precache-manifest.633b846a9c39b64de2926b85c8b3327d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d89fc923e777cf87a38df9e15b13352",
+    "url": "/divipay-challenge/index.html"
+  },
+  {
+    "revision": "d01b39fc818e2af4f980",
+    "url": "/divipay-challenge/static/css/main.897bae91.chunk.css"
+  },
+  {
+    "revision": "da187c673b5eebb3a2e6",
+    "url": "/divipay-challenge/static/js/2.0e0242e0.chunk.js"
+  },
+  {
+    "revision": "d01b39fc818e2af4f980",
+    "url": "/divipay-challenge/static/js/main.3b8d8a38.chunk.js"
+  },
+  {
+    "revision": "9349cea228b0cd8b3ef2",
+    "url": "/divipay-challenge/static/js/runtime~main.a7806d47.js"
+  }
+]);
