57ae2334f74666f4a1d088bfb36b984d78e33853
Alan Fidelino <afidedev26@gmail.com>
2019-07-07 21:58:22 +1000

    Initialize challenge and style table

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './App.css';
-import Table from './Table';
-import categories from './data/categories.json';
-import merchants from './data/merchants.json';
-import transactions from './data/transactions.json';
+import React from "react";
+import "./App.css";
+import Table from "./components/Table";
+import categories from "./data/categories.json";
+import merchants from "./data/merchants.json";
+import transactions from "./data/transactions.json";
 
 function App() {
-    return (
-        <div className="App">
-            <Table 
-              categories={categories} 
-              merchants={merchants} 
-              transactions={transactions}
-            />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Table
+        categories={categories}
+        merchants={merchants}
+        transactions={transactions}
+      />
+    </div>
+  );
 }
 
 export default App;
